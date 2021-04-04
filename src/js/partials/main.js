07efb635bbb08dd41e7fa00cd6dcb8917b59e261
ArtZ <snickgem@gmail.com>
2021-04-04 23:44:22 +0400

    adaptive

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -2,20 +2,82 @@ $(document).ready(function(){
 	// Phone mask
 	$("input[name='phone']").mask("+7 999 999 99 99");
 
+	// Anchors
+	anchorScroll($('.anchor'));
+
+	function anchorScroll(e) {
+		e.click(function () {
+			link = $(this).attr('href');
+			to = $(link).offset().top;
+			$('body, html').animate({
+				scrollTop: to
+			}, 800);
+		});
+	}
+	
+	// Header
+	$('.header__burger').click(function () {
+		$('.menu').addClass('open');
+		$('body').addClass('body-scroll-lock');
+	});
+
+	$('.menu__back').click(function () {
+		$('.menu').removeClass('open');
+		$('body').removeClass('body-scroll-lock');
+	});
+
 	// Set direction hover
-	$('.directions__item').hover(
-		function() {
-			$(this).find('svg').css('box-shadow', '0 0 0 6px ' + $(this).attr('data-color'));
+	if(window.matchMedia('(min-width: 768px)').matches){
+		$('.directions__item').hover(
+			function() {
+				$(this).find('svg').css('box-shadow', '0 0 0 6px ' + $(this).attr('data-color'));
 
-			// Change text
-			$('.directions__text').text($(this).attr('data-text'));
-		},
-		function() {
-			$(this).find('svg').removeAttr('style');
+				// Change text
+				$('.directions__text').text($(this).attr('data-text'));
+			},
+			function() {
+				$(this).find('svg').removeAttr('style');
 
-			$('.directions__text').text('ВЫБЕРИТЕ НАПРАВЛЕНИЕ');
+				$('.directions__text').text('ВЫБЕРИТЕ НАПРАВЛЕНИЕ');
+			}
+		);
+	}
+
+	// Directions slider
+	directionSliderInit();
+
+	$(window).resize(function () {
+		directionSliderInit();
+	});
+
+	function directionSliderInit() {
+		if(window.matchMedia('(max-width: 767px)').matches){
+			$('.directions__wrapper').slick({
+				slidesToShow: 2.2,
+				slidesToScroll: 1,
+				arrows: false,
+				infinite: false
+			});
 		}
-	);
+	}
+
+	// Filter slider
+	filterSliderInit();
+
+	$(window).resize(function () {
+		filterSliderInit();
+	});
+
+	function filterSliderInit() {
+		if(window.matchMedia('(max-width: 767px)').matches){
+			$('.filter').slick({
+				slidesToShow: 2.4,
+				slidesToScroll: 1,
+				arrows: false,
+				infinite: false
+			});
+		}
+	}
 
 	// Portfolio filter
 	setTimeout(function () {
